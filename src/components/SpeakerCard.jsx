fa36dd0274430e3049c0a3c31ba2ea76f0adf8f9
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-06-26 16:30:34 +0530

    text-xs in speakr card

--- src/components/SpeakerCard.jsx
+++ src/components/SpeakerCard.jsx
@@ -2,11 +2,8 @@
 const SpeakerCard = ({ speaker }) => {
   return (
     <div className="text-left max-h-[380px] overflow-hidden flex flex-col">
- 
-     
-
       <div className="overflow-y-auto pr-2 custom-scroll flex-grow">
-        <p className="text-sm text-gray-800 leading-relaxed whitespace-pre-line">
+        <p className="text-xs text-gray-800 leading-relaxed whitespace-pre-line">
           {speaker.description}
         </p>
       </div>
